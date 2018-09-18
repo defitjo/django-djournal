c029dd5518a65402ff97b46f2c52afaea2e73f3a
Devon <defitjo16@gmail.com>
2018-09-18 00:44:47 -0400

    templates

--- djangodjournal/djangodjournal/urls.py
+++ djangodjournal/djangodjournal/urls.py
@@ -20,5 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^$', views.home),
-    re_path(r'^about/$', views.about)
+    re_path(r'^about/$', views.about),
 ]
